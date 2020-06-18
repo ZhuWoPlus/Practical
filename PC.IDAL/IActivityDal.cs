302cf6ef36eaa51c9f39d6869a8dbe79bb2bb3ce
lenovo <lenovo@JULIA>
2020-06-18 11:10:21 +0800

    猪头

--- PC.IDAL/IActivityDal.cs
+++ PC.IDAL/IActivityDal.cs
@@ -10,6 +10,7 @@ namespace PC.IDAL
     {
         /// <summary>
         /// 显示查询活动列表
+        /// 
         /// </summary>
         /// <returns>通过</returns>
         List<Activity_DoTableModel> Show(string name);
